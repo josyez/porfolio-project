1a15f15121cfdb3699a571bbecd997b5dff286f0
Joselyn <cuzco.joselyn@gmail.com>
2023-04-07 18:41:00 -0400

    chnages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300 ">
       <div>
-        <img src={Logo} alt="Logo image" style={{ width: "50px" }} />
+        <img src={Logo} style={{ width: "50px" }} />
       </div>
 
       {/* menu */}
